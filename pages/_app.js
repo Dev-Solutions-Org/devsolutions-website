bd09aed8dd4648b0803e40a9b588a4caaac8ea0f
Daniel Esteves <estevesd8@gmail.com>
2019-08-29 20:39:45 -0400

    first commit

---
+++ pages/_app.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import App from 'next/app'
+import Head from 'next/head'
+
+class MyApp extends App {
+  // Only uncomment this method if you have blocking data requirements for
+  // every single page in your application. This disables the ability to
+  // perform automatic static optimization, causing every page in your app to
+  // be server-side rendered.
+  //
+  // static async getInitialProps(appContext) {
+  //   // calls page's `getInitialProps` and fills `appProps.pageProps`
+  //   const appProps = await App.getInitialProps(appContext);
+  //
+  //   return { ...appProps }
+  // }
+
+  render () {
+    const { Component, pageProps } = this.props
+    return (
+      <>
+        <Head>
+          <link rel='stylesheet' href='https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css' integrity='sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T' crossOrigin='anonymous' />>
+        </Head>
+        <Component {...pageProps} />
+      </>
+    )
+  }
+}
+
+export default MyApp
