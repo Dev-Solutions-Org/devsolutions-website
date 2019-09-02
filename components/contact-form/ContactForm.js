eeff116eb089ed89f3295cc017a178eab67c0fd7
Carls13 <charlesshb98@gmail.com>
2019-09-02 14:38:51 -0400

    Contact page

---
+++ components/contact-form/ContactForm.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import './contact-form.styles.css';
+
+class ContactForm extends React.Component{
+	state = {
+		name: '',
+		email: '',
+		message: '',
+	};
+
+ render(){
+
+  return(
+		   <div className="col-12 col-lg-6 mb-0 form" style={{
+		   	backgroundImage: "url('./../../static/ds-icon.png')"
+		   }}>
+				<input type="text" name="name" placeholder="Full Name" className="mt-5 mb-3" id="nameForm" />
+				<input type="email" name="email" placeholder="E-mail" className="mb-3" id="emailForm" />
+				<textarea name="message" placeholder="Your Message" id="messageForm"></textarea>
+				<button className="mt-3 mb-5" id="button-send" onclick="sendContactMail()">
+				Enviar
+				</button>
+			</div>
+  );
+ }
+
+}
+
+export default ContactForm;
--- pages/contact.js
+++ pages/contact.js
@@ -1,5 +1,6 @@
 // import { Link } from '../routes'
 import Layout from './../components/layout/Layout';
+import ContactForm from './../components/contact-form/ContactForm';
 
 class Index extends React.Component{
 
@@ -11,7 +12,13 @@ class Index extends React.Component{
 
     return(
       <Layout title='Contact | Dev Solutions'>    
-      	<h1 className="text-center"><b>CONTACT</b></h1>  
+      	<h1 className="text-center"><b>CONTACT</b></h1>
+      	<div className="row">
+      		<ContactForm/>
+      		<div className="col-12 col-lg-6 mb-3">
+      			<img src="./../static/green-logo.jpg" className="p-3 img-contact"/>
+      		</div>
+      	</div>
       </Layout>)
   }
 }
