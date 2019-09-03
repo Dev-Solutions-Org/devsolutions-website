4dea9c7283c08da3d8c50455c7c371b9d7c0b935
Daniel Esteves <estevesd8@gmail.com>
2019-09-02 20:53:53 -0400

    route for pay

--- routes.js
+++ routes.js
@@ -1,8 +1,9 @@
 const routes = require('next-routes')
-                                                  
-module.exports = routes()                           
-.add('index')                                       
-.add('services')                                       
-.add('portfolio')                                       
-.add('team')                                       
-.add('contact')                                       
+
+module.exports = routes()
+  .add('index')
+  .add('services')
+  .add('portfolio')
+  .add('team')
+  .add('contact')
+  .add('pay')
