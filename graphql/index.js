dc5b6d8b02922fecadd131b858b52eacc78c5d3e
Daniel Esteves <estevesd8@gmail.com>
2019-09-10 15:59:23 -0400

    api url

--- graphql/index.js
+++ graphql/index.js
@@ -3,7 +3,7 @@ import { useGraphQL } from 'graphql-react'
 export function getMembersTeam () {
   const { loading, cacheValue: { data, ...errors } = {} } = useGraphQL({
     fetchOptionsOverride (options) {
-      options.url = `${process.env.API_URL}`
+      options.url = 'http://api.devsolution.us/graphql'
     },
     operation: {
       query: /* GraphQL */ `
